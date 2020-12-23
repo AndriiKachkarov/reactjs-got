1346c6a78de48e7d1aa12de58d4ae60ae2d495b9
Andrii Kachkarov <Andriy.Kachkarov@gmail.com>
2020-12-23 11:47:08 +0200

    edit gotService, add random char showing

--- src/services/gotService.js
+++ src/services/gotService.js
@@ -13,11 +13,58 @@
         return await res.json();
     }
 
-    getAllChars() {
-        return this.getResource('/characters?page=5&pageSize=10');
+    async getAllChars() {
+        const chars = await this.getResource('/characters?page=5&pageSize=10');
+        return chars.map(char => this._transformChar(char));
     }
 
-    getChar(id) {
-        return this.getResource(`/characters${id}`);
+    async getChar(id) {
+        const char = await this.getResource(`/characters/${id}`);
+        return this._transformChar(char);
     }
+
+    async getAllHouses() {
+        const houses = await this.getResource('/houses/');
+        return houses.map(this._transformHose);
+    }
+
+     async getHouse(id) {
+         const house = await this.getResource(`/houses/${id}`);
+         return this._transformHose(house);
+     }
+
+     async getAllBooks() {
+         const books = await this.getResource('/books/');
+         return books.map(this._transformBook());
+     }
+
+     _transformChar(char) {
+        return {
+            name: char.name,
+            gender: char.gender,
+            born: char.born,
+            died: char.died,
+            culture: char.culture
+        }
+     }
+
+     _transformHose(house) {
+         return {
+             name: house.name,
+             region: house.region,
+             words: house.words,
+             titles: house.titles,
+             overlord: house.overlord,
+             ancestralWeapons: house.ancestralWeapons
+         }
+     }
+
+     _transformBook(book) {
+         return {
+             name: book.name,
+             numberOfPages: book.numberOfPages,
+             publiser: book.publiser,
+             released: book.released,
+         }
+     }
 }
